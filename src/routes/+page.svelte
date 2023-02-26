3568695493f5058ebfba90a28cae7751345e6cd9
Nikita <nikitapashinsky@gmail.com>
2023-02-26 23:07:17 +0300

    able to add todos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<script>
+	import { todos } from '../stores.js';
+	let newTodo = '';
+
+	$: console.log($todos);
+	function handleSubmit() {
+		$todos = [newTodo, ...$todos];
+		newTodo = '';
+	}
+
+	function handleRemove(index) {
+		$todos.splice(index, 1);
+		$todos = $todos;
+	}
+</script>
+
+<main class="p-8 flex flex-col gap-4">
+	<h1 class="font-medium">Todos</h1>
+	<form on:submit|preventDefault={handleSubmit}>
+		<input
+			type="text"
+			bind:value={newTodo}
+			placeholder="Add todo"
+			class="text-sm focus:outline-none"
+		/>
+	</form>
+	<ul>
+		{#each $todos as todo, index (todo)}
+			<li class="text-sm" on:click={() => handleRemove(index)}>{todo}</li>
+		{/each}
+	</ul>
+</main>
